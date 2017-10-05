0806c355751109b8a9ccb1a44bd120a84eae3c32
Kamal Mango <kamal.mango7@gmail.com>
2017-10-05 11:02:20 +0300

    complete

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { 
+  getMetricMetaInfo, 
+  timeToString, 
+  getDailyReminderValue,
+  clearLocalNotification,
+  setLocalNotification 
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -10,6 +16,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -76,10 +83,12 @@ class AddEntry extends Component {
       [key]: entry
     }))
 
-    // Navigate to home
+    this.toHome()
 
     submitEntry({ key, entry })
-    // clar local notification
+
+    clearLocalNotification()
+      .then(setLocalNotification)
   }
 
   reset = () => {
@@ -89,9 +98,15 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }))
 
-    // route to home
+    this.toHome()
 
     removeEntry(key)
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: 'AddEntry'
+    }))
   }
 
   render () {
